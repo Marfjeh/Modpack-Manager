f80b17e5d164e4f8ec643099ff6c453c54851bf3
marfjeh <git@marfprojects.nl>
2019-09-24 22:28:29 +0200

    Added Packages, and some basic models.
    Signed-off-by: marfjeh <git@marfprojects.nl>

---
+++ src/app/Http/Controllers/ModpackChangelogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\modpack_changelog;
+use Illuminate\Http\Request;
+
+class ModpackChangelogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\modpack_changelog  $modpack_changelog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(modpack_changelog $modpack_changelog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\modpack_changelog  $modpack_changelog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(modpack_changelog $modpack_changelog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\modpack_changelog  $modpack_changelog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, modpack_changelog $modpack_changelog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\modpack_changelog  $modpack_changelog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(modpack_changelog $modpack_changelog)
+    {
+        //
+    }
+}
---
+++ src/app/Http/Controllers/ModpackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\modpack;
+use Illuminate\Http\Request;
+
+class ModpackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\modpack  $modpack
+     * @return \Illuminate\Http\Response
+     */
+    public function show(modpack $modpack)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\modpack  $modpack
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(modpack $modpack)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\modpack  $modpack
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, modpack $modpack)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\modpack  $modpack
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(modpack $modpack)
+    {
+        //
+    }
+}
